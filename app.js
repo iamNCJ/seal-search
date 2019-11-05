0eb1a04a51a3e8a1e954ea97c7960eea4a07ab09
nessoffice <1297295485@qq.com>
2019-11-05 13:15:57 +0800

    19-11-05: add mysql connection, ajax_bug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mysql   = require('mysql');
 const express = require('express');
 const urlJoin = require('url-join');
 
@@ -14,6 +15,33 @@ app.get(fullURL('/hello'), (req, res) => {
   res.statusCode = 200;
   res.end('hello world!');
 });
+
+var connection = mysql.createConnection({ 
+ host   : '123.56.17.42', 
+ user   : 'small_seal_visitor', 
+ password : 'THE_password123', 
+ database : 'small_seal'
+}); 
+  
+connection.connect();
+var sql = 'SELECT * FROM small_seal_content';
+var data = '';
+connection.query(sql, function(err, rows, fields) { 
+  if (err) {
+    console.log(err);
+    return;
+  };
+  var dataString = JSON.stringify(rows);
+  data = JSON.parse(dataString);
+  // console.log('The solution is: ', data); 
+});
+app.get(fullURL('/query'), (req, res) => {
+  res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});//设置response编码为utf-8
+  // console.log('query done');
+  res.end('query');
+});
+
+connection.end(); 
 
 app.listen(port, () => {
   console.log(`Server running at ${hostname}:${port}`)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# to-do list
+
+1.express+mysql实现查询功能
+
+2.将图片信息放到MySQL上
+
+3.美化前端（materialize）
+
